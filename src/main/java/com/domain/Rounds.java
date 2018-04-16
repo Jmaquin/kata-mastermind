c4fb37626062f7f60fce189b503d503665fc8863
Johann Maquin <johann.maquin@gmail.com>
2018-04-16 15:05:18 +0200

    Update error management on custom lists

--- src/main/java/com/domain/Rounds.java
+++ src/main/java/com/domain/Rounds.java
@@ -1,7 +1,6 @@
 package com.domain;
 
 import com.Exception.EmptyRoundListException;
-import com.Exception.LastRoundReachedException;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -11,17 +10,8 @@ import java.util.ArrayList;
 public class Rounds extends ArrayList<Round> {
     @Getter private int maxSize;
 
-    /**
-     * Unckecked exception is thrown because we don't want to add more than maxSize elements and throw a checked exception is not possible when overriding add method
-     *
-     * @param round Round to be added to the list
-     * @return true when round is added
-     */
     public boolean add(Round round) {
-        if (this.size() == maxSize)
-            throw new LastRoundReachedException("Last round already reached!");
-        else
-            return super.add(round);
+        return this.size() < maxSize && super.add(round);
     }
 
     Round getCurrentRound() throws EmptyRoundListException {
@@ -31,7 +21,7 @@ public class Rounds extends ArrayList<Round> {
             return this.get(this.size() - 1);
     }
 
-    void createNextRound(PawnsSelection userPawns, int correctlyPlacedPawns, int misplacedPawns) {
-        this.add(new Round(userPawns, correctlyPlacedPawns, misplacedPawns));
+    boolean createNextRound(PawnsSelection userPawns, int correctlyPlacedPawns, int misplacedPawns) {
+        return this.add(new Round(userPawns, correctlyPlacedPawns, misplacedPawns));
     }
 }
